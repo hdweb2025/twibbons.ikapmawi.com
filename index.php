dcc421908c7e629db517babc9b42ed88d5489cf3
Husen Nh <hd.website19@gmail.com>
2026-03-21 02:25:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -74,7 +74,7 @@ $events = mysqli_query($conn, "SELECT * FROM events ORDER BY created_at DESC");
                 ?>
 
                 <div class="prompt-actions">
-                    <a href="<?php echo $register_url; ?>" class="btn-primary">Daftar Alumni</a>
+                    <a href="<?php echo $register_url; ?>" class="btn-primary">Buat Twibbon</a>
                     <div class="divider"><span>atau</span></div>
                     <a href="<?php echo $login_url; ?>" class="btn-link">Sudah punya akun? <b>Masuk</b></a>
                 </div>
